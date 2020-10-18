2154c7b4d82084b52a5088ee4ae2b0d6f110b9fd
Jhon Guerrero <jhoguer@gmail.com>
2020-10-18 00:20:36 -0500

    Creando modelo de productos, registrando productos y listando todos los productos

--- src/app.js
+++ src/app.js
@@ -8,7 +8,9 @@ const app = express()
 
 app.set('pkg', pkg);
 
+
 app.use(morgan('dev'));
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,9 +1,20 @@
-export const createProduct = (req, res) => {
+import Product from '../models/Product'
 
+export const createProduct = async (req, res) => {
+
+  const { name, category, price, imgURL } = req.body
+
+  const newProduct = new Product( { name, category, price, imgURL } )
+  const productSaved = await newProduct.save()
+
+  res.status(201).json( productSaved )
+ 
 }
 
-export const getProducts = (req, res) => {
-  res.json("get productsssss")
+export const getProducts = async (req, res) => {
+  
+  const products = await Product.find()
+  res.json( products )
 }
 
 export const getProductById = (req, res) => {
